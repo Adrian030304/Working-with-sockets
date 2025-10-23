373b14a250dcb956e3eeb8e0b201490978566f29
Adrian030304 <adrianromanenco1@gmail.com>
2025-10-23 09:13:27 +0300

    fixtures

--- src/client.py
+++ src/client.py
@@ -1,6 +1,8 @@
 import socket
 from log import logger
 
+HEADER_SIZE = 10
+
 s= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 # the client contrary to the server, instead of binding it just straight up connects
@@ -10,11 +12,24 @@ s.connect((socket.gethostname(), 1234))
 # accept the message
 
 # you can buffer data meaning that you can send it in small packages instead
+
 while True:
-    msg = s.recv(8)
-    decoded_message = msg.decode("utf-8")
-    # print(decoded_message)
-    if len(msg) <= 0:
-        break
-    logger.info(decoded_message)
+
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(16)
+        # print(decoded_message)
+        if new_msg:
+            logger.info(f"New message length: {msg[:HEADER_SIZE]}")
+            msglen = int(msg[:HEADER_SIZE])
+            new_msg = False
+
+        full_msg += msg.decode("utf-8")
+        if len(full_msg) - HEADER_SIZE == msglen:
+            logger.info("Full message rcvd")
+            logger.info(full_msg[HEADER_SIZE:])
+            new_msg = True
+            full_msg = ''
+    print(full_msg)
     
--- src/server.py
+++ src/server.py
@@ -3,6 +3,13 @@ from log import logger
 # Socket it is lika a two way endpoint that receives data and sends data
 
 
+# msg = "Welcome to the message!"
+# # creating a fixed length header
+# print(f"{len(msg):<20}"+msg)
+
+HEADER_SIZE = 10
+
+# if ran on a webserver then bind to the ip address you can find with ipconfig and you have to connect to a public ip address
 s= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 s.bind((socket.gethostname(), 1234))
@@ -16,5 +23,8 @@ while True:
     # print(f"Connection from {address} has been established!")
     logger.info(f"Connection from {address} has been established!")
     # sends information to the client socket
-    clientsocket.send(bytes("Welcome to the server", "utf-8"))
-    clientsocket.close()
+    msg = "Welcome to the server!"
+    # creating a fixed length header
+    msg = f"{len(msg):<{HEADER_SIZE}}" + msg
+
+    clientsocket.send(bytes(msg, "utf-8"))
