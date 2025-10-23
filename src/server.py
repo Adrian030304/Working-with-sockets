70b6449c3e578b58d1a5b82ffdb929b6f6f2b5d5
Adrian030304 <adrianromanenco1@gmail.com>
2025-10-23 09:46:32 +0300

    server init

--- src/server.py
+++ src/server.py
@@ -1,30 +1,58 @@
 import socket
-from log import logger 
-# Socket it is lika a two way endpoint that receives data and sends data
-
-
-# msg = "Welcome to the message!"
-# # creating a fixed length header
-# print(f"{len(msg):<20}"+msg)
+import select
 
 HEADER_SIZE = 10
+ip = "127.0.0.1"
+PORT = 1234
 
-# if ran on a webserver then bind to the ip address you can find with ipconfig and you have to connect to a public ip address
-s= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.bind((socket.gethostname(), 1234))
+# this allows us to reconnect
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-# here basically the server will open and listen for incoming connections
-s.listen(5)
+server_socket.bind((ip,PORT))
 
-# this loop is when it is listening for connections
+server_socket.listen()
+
+sockets_list = [server_socket]
+
+clients = {}
+
+def receive_message(client_socket):
+    try:
+        message_header = client_socket.recv(HEADER_SIZE)
+        if not len(message_header):
+            return False
+        message_length = int(message_header.decode("utf-8").strip())
+        return {"header": message_header, "data": client_socket.recv(message_length)}
+    except:
+        return False
+    
 while True:
-    clientsocket, address = s.accept()
-    # print(f"Connection from {address} has been established!")
-    logger.info(f"Connection from {address} has been established!")
-    # sends information to the client socket
-    msg = "Welcome to the server!"
-    # creating a fixed length header
-    msg = f"{len(msg):<{HEADER_SIZE}}" + msg
+    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
 
-    clientsocket.send(bytes(msg, "utf-8"))
+    for notified_socket in read_sockets:
+        if notified_socket == server_socket:
+            client_socket, client_address = server_socket.accept()
+
+            user = receive_message(client_socket)
+            if user is False:
+                continue
+            sockets_list.append(client_socket)
+
+            clients[client_socket] = user
+
+            print(f"Accepted new connection from {client_address[0]}:{client_address[1]} username:{user['data'].decode('utf-8')}")
+        else:
+            message = receive_message(notified_socket)
+            if message is False:
+                print(f"Closed connection from {clients[notified_socket]['data'].decode('utf-8')}")
+                sockets_list.remove(notified_socket)
+                del clients[notified_socket]
+                continue
+            user = clients[notified_socket]
+            print(f"Received message from {user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}")
+
+            for client_socket in clients:
+                if client_socket != notified_socket:
+                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
